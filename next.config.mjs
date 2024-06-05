eebe5c4d4531ca4cc4a3f29cf86ba4dc467b8930
petrecirceagwork <petrecirceagwork@gmail.com>
2024-06-05 11:44:52 +0000

    gave up carousel with tailwindcss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['picsum.photos'], // Add your allowed domain here
+      },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import CarouselComponent from '../Components/Carousel'
+import Image from "next/image"
 
 export default function Home() {
   return (
-      <CarouselComponent/>
+    <div className="container mx-auto px-4 py-8">
+      <div className="container d-flex">
+        <h1>Title</h1>
+        <p>Text</p>
+      </div>
+        <Image
+          src="https://picsum.photos/200/300"
+          width={300}
+          height={200}
+          alt="test image"
+        />
+    </div>
   );
 }
