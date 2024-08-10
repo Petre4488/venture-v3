2f47eceebe41d23d48193b92d9c8e1239ec7fb19
Petre4488 <petrecirceagwork@gmail.com>
2024-08-10 19:55:56 +0300

    stripe keys update on vercel

--- src/Components/ToS.tsx
+++ src/Components/ToS.tsx
@@ -15,7 +15,7 @@ export default function ToS() {
           <div className="space-y-6">
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
               With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-              companies around the world are updating their terms of service agreements to comply.
+              companies around the world are updating their terms of service agreements to comply..
             </p>
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
               The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
